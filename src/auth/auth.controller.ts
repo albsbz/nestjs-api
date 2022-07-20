da2a408a0cb8a93290219b2b5be877f7ab0f9b68
Albsbz <best.shop.bot@gmail.com>
2022-07-20 11:02:39 +0300

    add logout and fix refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Request as RequestType } from 'express';
 import { User } from 'src/users/schemas/user.schema';
 import { AuthService } from './auth.service';
+import { LoginResponse } from './dto/responses.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
@@ -9,7 +13,24 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Request() req): Promise<User> {
-    return this.authService.login(req.user);
+  async login(
+    @Request() req: RequestType & { user: User },
+  ): Promise<LoginResponse> {
+    return this.authService.createTokens(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  async getProfile(@Request() req): Promise<void> {
+    return this.authService.saveEmptyRefreshToken(req.user.userId);
+  }
+
+  @UseGuards(JwtRefreshGuard)
+  @Post('refresh')
+  async refresh(@Request() req): Promise<LoginResponse> {
+    return this.authService.compareRefreshToken(
+      req.user.refreshToken,
+      req.user.userId,
+    );
   }
 }
